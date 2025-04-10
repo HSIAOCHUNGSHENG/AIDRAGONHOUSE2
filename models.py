6134e0c90b2c89db1e4968e176596dc3c7a0fe1b
HSIAOCHUNGSHENG <fet0989147988@gmail.com>
2025-04-10 20:51:33 +0800

    Add files via upload

--- models.py
+++ models.py
@@ -73,6 +73,20 @@ class News(db.Model):
     def __repr__(self):
         return f'<News {self.title}>'
 
+class Portfolio(db.Model):
+    """作品展示模型"""
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image_path = db.Column(db.String(255), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    order = db.Column(db.Integer, default=0)  # 用於自定義排序
+    active = db.Column(db.Boolean, default=True)
+    
+    def __repr__(self):
+        return f'<Portfolio {self.title}>'
+
 class ContactInfo(db.Model):
     """聯絡資訊模型"""
     id = db.Column(db.Integer, primary_key=True)
